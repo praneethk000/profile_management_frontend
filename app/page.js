30df50b6a555eee15f1086e117b48bf6d910afc0
praneethk000 <praneethkpoojary000@gmail.com>
2025-09-26 06:52:20 +0530

    data fetching

--- app/page.js
+++ app/page.js
@@ -1,33 +1,9 @@
-// import ProfileCard from "./components/ProfileCard";
-// import { getAllProfiles } from "./services/profileService";
-
-// export default async function Home() {
-//   const profiles = await getAllProfiles();
-
-//   return (
-//     <div style={{ padding: "32px" }} className="bg-white text-black">
-//       <h1 className="text-2xl font-semibold mb-5">All Profiles</h1>
-//       {profiles.length === 0 ? (
-//         <p>No profiles found.</p>
-//       ) : (
-//         profiles.map((profile) => (
-//           <ProfileCard key={profile.id} profile={profile} />
-//         ))
-//       )}
-//     </div>
-//   );
-// }
-
 import ProfileCard from "./components/ProfileCard";
 import { getAllProfiles } from "./services/profileService";
 
 export default async function Home() {
-  let profiles = [];
-  try {
-    profiles = await getAllProfiles();
-  } catch (error) {
-    console.error("SSR fetch failed:", error.message);
-  }
+  const profiles = await getAllProfiles();
+  console.log(profiles);
 
   return (
     <div style={{ padding: "32px" }} className="bg-white text-black">
